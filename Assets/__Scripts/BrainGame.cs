e7df1663c07cced8de3377368998f0e71ce8770a
Jasper Holland <83934607+UnicornSyrup@users.noreply.github.com>
2024-10-27 12:40:16 -0500

    UI Scaling fixes

--- Assets/__Scripts/BrainGame.cs
+++ Assets/__Scripts/BrainGame.cs
@@ -3,8 +3,6 @@ using System.Collections.Generic;
 using UnityEngine;
 using TMPro;
 using UnityEngine.UI;
-using UnityEditorInternal;
-using Unity.VisualScripting;
 using TimeSpan = System.TimeSpan;
 using UnityEngine.SceneManagement;
 
